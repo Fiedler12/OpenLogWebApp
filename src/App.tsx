196264c42c5dd2d394c226b1023b99841d073b95
tuborgstroem <tborgstroem@gmail.com>
2022-03-21 16:22:22 +0100

    added page log cycle

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { AboutUs } from './pages/AboutUs';
 import { Home } from './pages/Home';
 import { Layout } from './pages/Layout';
+import {LogCycle} from './pages/LogCycle' ;
 
 //testing push
 function App() {
@@ -11,6 +12,8 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} /> 
           <Route path="about-us" element={<AboutUs />} />
+          <Route path="log-cycle" element={<LogCycle />} />
+          
         </Route>
       </Routes>
     </BrowserRouter>
