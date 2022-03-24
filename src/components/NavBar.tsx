31228f17b5fadb75ff906e0eb5b2a0b70f51e199
chen0046 <chen0046@hotmail.com>
2022-03-24 15:51:01 +0100

    logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,20 +1,30 @@
-import { AppBar, IconButton, Toolbar } from "@mui/material";
+import { AppBar, Avatar, IconButton, Toolbar } from "@mui/material";
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
     return (
+    
         <AppBar position="static">
+
             <Toolbar>
-                
+                <IconButton>
+                <Link to="/">
+                <img src="../Logo.png"> 
+                </img></Link>
+                </IconButton>
+
                 <IconButton>
                     <Link to="/">Home</Link>
                 </IconButton>
+
                 <IconButton>
                     <Link to="/about-us">AboutUs</Link>
                 </IconButton>
+
                 <IconButton>
                     <Link to="/log-cycle">LogCycle</Link>
                 </IconButton>
+
                 <IconButton>
                     <Link to="/Settings">Settings</Link>
                 </IconButton>
