085f9af497983d81ab4d8fbf4ffdb435b0a03916
Chenxi Cai <s205420@dtu.dk>
2022-04-11 13:33:28 +0200

    aboutus design

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -1,8 +1,32 @@
+import { Container, Grid } from '@mui/material';
 import React from 'react'
+import '../css_files/AboutUs.css';
+import img from '../Pictures/App.png';
+
+
 
 export const AboutUs = () => {
   return (
-    <div>AboutUs</div>
+    <div>
+      <h1 className='header'>What is OpenLog</h1>
+      <p className='text'>OpenLog allows you to log and keep track
+      of any disired data
+      You can costumize your units and categories of your choosing</p>
+      <h2 className='header'>About our APP</h2>
+     <img src={img} className = 'image'/>
+
+
+
+     <div >
+      
+     </div>
+     
+
+
+    </div>
+
+    
+  
   )
 }
 
