048e3b3213f7149f008911dcfb716960d1660ee4
Chenxi Cai <s205420@dtu.dk>
2022-04-11 15:31:24 +0200

    aboutus

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -13,12 +13,37 @@ export const AboutUs = () => {
       of any disired data
       You can costumize your units and categories of your choosing</p>
       <h2 className='header'>About our APP</h2>
-     <img src={img} className = 'image'/>
-
-
-
-     <div >
+      <img src={img} className = 'image'/>
       
+    
+
+
+
+     <div className='format' >
+       <ul className='container'>
+         <li className='text2'>Creator</li>
+         <li>Alexander Solomon S201172</li>
+         <li>Chenxi Cai S205420</li>
+         <li>Isabel G.Jacobsen S205473</li>
+         <li>Oliver Fiedler S205423</li>
+         <li>Tobias L. Borgstørm S184810 </li>
+
+       </ul>
+       <ul className='container'> 
+         <li className='text2'>Features</li>
+         <li>Log data</li>
+         <li>See previous data</li>
+         <li>delete/save data</li>
+         <li>Edit data</li>
+         
+       </ul>
+       <ul className='container'>
+         <li className='text2'>Contact</li>
+         <li>22222222</li>
+         <li>Openlogsvej 1.</li>
+         <li>openlog@gmail.com</li>
+       </ul>
+
      </div>
      
 
