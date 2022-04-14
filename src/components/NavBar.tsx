d68d9e0b9f6c8c07e2e2d34d21376f6b6dacf376
alexandersolomon <alexsolomon91@gmail.com>
2022-04-14 10:30:22 +0200

    alex navbar change with box

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { AppBar, Avatar, IconButton, Toolbar } from "@mui/material";
+import { AppBar, Avatar, IconButton, Toolbar, Box, Typography, Button } from "@mui/material";
 import { Link } from "react-router-dom";
 import '../css_files/navbar.css'
 
@@ -7,19 +7,23 @@ import '../css_files/navbar.css'
 
 const NavBar = () => {
     return (
-    
-        <AppBar position="static" className="bigblue" sx={{height:'100px'}} color="primary">
+        <AppBar position="static" className="bigblue" sx={{height:'100px'}} >
             <Toolbar>
-                <IconButton>
+                <Box sx={{ flexGrow: 1}}>  
+                <IconButton  edge="start" >
                 <Link to="/">
                 <img src="../Logo.png " className="logo"> 
                 </img></Link>
                 </IconButton>
+                <Button color="inherit">Login</Button>
+                </Box>
 
+
+                <Box>
                 <button className="menu">
                     <Link to="/" style={{textDecoration: 'none'}}>Home</Link>
                 </button>
-
+               
                 <button className="menu">
                     <Link to="/about-us" style={{textDecoration: 'none'}}>AboutUs</Link>
                 </button>
@@ -35,13 +39,15 @@ const NavBar = () => {
                 <button className="menu">
                     <Link to="/Login" style={{textDecoration: 'none'}}>Login</Link>
                 </button>
-                <IconButton >
+                <IconButton>
                 <Link to="/">
                 <img src="../account.png " className="accountlogo"> 
                 </img></Link>
                 </IconButton>
+                </Box>  
             </Toolbar>
         </AppBar>
+        
     )
 }
 
