c1ef1cf5cfe1bc62d7871a76ce1a19e2762bb631
Fiedler12 <o.lyngholm@gmail.com>
2022-04-22 15:26:20 +0200

    i dont know

--- src/pages/AddNewLog.tsx
+++ src/pages/AddNewLog.tsx
@@ -1,22 +1,16 @@
-<<<<<<< HEAD
 
 import { collection, Firestore, getDocs } from 'firebase/firestore/lite';
 import React, { useState } from 'react'
 import { database } from '../components/Firebase';
 import {ref, set, push} from "firebase/database"; 
 import  userId from "./LoginPage"
-
-=======
-import React from 'react'
 import { AddNewLogSetup } from '../components/AddNewLogSetup'
->>>>>>> d497b8f28f2bf25d17caf92b7da618dc7acbcef8
 
 export const AddNewLog = () => {
     const db = database; 
     const [logName, setLogName] = useState("Name")
     const [logMeasure, setLogMeasure] = useState("Measure")
     return (
-<<<<<<< HEAD
         <>
         <form onSubmit={(e) => {
             e.preventDefault();
@@ -38,9 +32,5 @@ export const AddNewLog = () => {
             <br />
             <button type='submit'>Create</button>
         </form></>
-=======
-        <AddNewLogSetup/>
-   
->>>>>>> d497b8f28f2bf25d17caf92b7da618dc7acbcef8
     )
 }
