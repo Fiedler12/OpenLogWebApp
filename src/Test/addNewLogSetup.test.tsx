2680456ae0644bb2c4a87c6f1bbbc26137f83cae
Fiedler12 <o.lyngholm@gmail.com>
2022-05-09 19:34:53 +0200

    logoverview changes

--- src/Test/addNewLogSetup.test.tsx
+++ src/Test/addNewLogSetup.test.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import ReactDOM from 'react-dom';
-import { AddNewLogSetup } from '../components/AddNewLogSetup';
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<AddNewLogSetup></AddNewLogSetup>,div)
 
 })
--- src/pages/LogOverview.tsx
+++ src/pages/LogOverview.tsx
@@ -25,13 +25,13 @@ interface value {
 
 
 export const LogOverview = () => {
-  const logId = useParams().id
-  const [id, setId] = useState('')
-  let log: log | undefined;
+  const logId = useParams().id; 
+  const [id, setId] = useState('');
+  const [log, setLog] = useState<log>(); 
   const current = new Date();
-  const [values, setValues] = useState<value[]>([])
-  const [newValue, setNewValue] = useState('')
-  const [logs, setLogs] = useState<log[]>()
+  const [values, setValues] = useState<value[]>([]);
+  const [newValue, setNewValue] = useState('');
+  const [logs, setLogs] = useState<log[]>();
 
   async function getValues() {
     let currentValues = await DatabaseService.getValues(); 
@@ -46,8 +46,11 @@ export const LogOverview = () => {
 
   useEffect(() => {
     getLogs();
-    log = logs?.find((n: any) => n.id === Number(logId))
   }, []);
+
+  useEffect(() => {
+    setLog(logs?.find((n: any) => n.id === Number(logId)))
+  }, [logs])
 
   useEffect(() => {
     getValues();
